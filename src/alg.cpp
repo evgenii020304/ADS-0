a8e3d3c7f9c2fc0fc457860d5319c777b7dcbf71
Evgeniy Naumov <63817948+evgenii020304@users.noreply.github.com>
2023-02-18 19:04:14 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -4,11 +4,9 @@ int gcd(int a, int b) {
 	if (a == b) {
 		return a;
 	}
-	else {
-		if (a > b) {
-			return gcd(a - b, b);
-		} else {
-			return gcd(a, b - a);
-		}
+	if (a > b) {
+		return gcd(a - b, b);
+	} else {
+		return gcd(a, b - a);
 	}
 }
