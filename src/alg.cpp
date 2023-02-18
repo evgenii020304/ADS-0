6f14a2b60c8dfba5c2cda59b946a53230e0d4fc4
Evgeniy Naumov <63817948+evgenii020304@users.noreply.github.com>
2023-02-18 18:57:47 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,5 +1,14 @@
 // Copyright 2021 NNTU-CS
 
 int gcd(int a, int b) {
-    // поместить сюда текст реализации алгоритма (на С++)
+	if (a == b) {
+		return a;
+	}
+	else {
+		if (a > b) {
+			return gcd(a - b, b);
+		} else {
+			return gcd(a, b - a);
+		}
+	}
 }
